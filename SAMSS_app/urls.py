c52baa3bf108a2e6cb6bbce28a700430dcad760b
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2023-11-29 09:12:39 +0545

    feat;all part add

--- SAMSS_app/urls.py
+++ SAMSS_app/urls.py
@@ -3,4 +3,12 @@ from SAMSS_app import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("courses/", views.CoursesView.as_view(), name="courses"),
+    path("teacher", views.TeacherView.as_view(), name="teacher"),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path("pricing/", views.PricingView.as_view(), name="pricing"),
+    path("blog/", views.BlogView.as_view(), name="blog"),
+    path("contact/", views.ContactView.as_view(), name="contact"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("courses_create/", views.CouresCreateView.as_view(), name="courses_create"),
 ]
