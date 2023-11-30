4057829b8ef9a74d99e949dbe614a34bf4995dd9
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2023-11-30 21:02:52 +0545

    feat;partition in website

--- SAMSS_app/views.py
+++ SAMSS_app/views.py
@@ -6,17 +6,17 @@ class HomeView(TemplateView):
     template_name = "SAMSS/index.html"
 
 class CoursesView(TemplateView):
-    template_name = "courses.html"
+    template_name = "SAMSS/courses.html"
 
 
 class TeacherView(TemplateView):
-    template_name = "techer.html"
+    template_name = "SAMSS/teacher.html"
 
 class AboutView(TemplateView):
     template_name = "SAMSS/about.html"
 
 class PricingView(TemplateView):
-    template_name = "pricing.html"
+    template_name = "SAMSS/pricing.html"
     
 class BlogView(TemplateView):
     template_name = "SAMSS/blog.html"
@@ -25,8 +25,8 @@ class ContactView(TemplateView):
     template_name = "SAMSS/contact.html"
     
 class LoginView(TemplateView):
-    template_name = "login.html"
+    template_name = "SAMSS/login.html"
     
 class CouresCreateView(TemplateView):
-    template_name = "courescreate.html"
+    template_name = "SAMSS/courescreate.html"
     
